cb799b5823ad6ab12ea4d756ac17c8a5a233fb53
Sirx <bradx2713@gmail.com>
2024-02-29 11:32:59 +0200

    Initial Commit

---
+++ MacTracker_Application/MacTracker_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTracker_ApplicationApp.swift
+//  MacTracker_Application
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct MacTracker_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
